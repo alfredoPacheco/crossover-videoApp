e8d1e050391617d9e7a9581535116789f9b6f28e
apacheco <j.alfredo.pacheco@gmail.com>
2016-09-09 08:42:18 -0600

    -Backend installed. -Frontend scaffolded.

---
+++ frontend/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name videosApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the videosApp
+ */
+angular.module('videosApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
